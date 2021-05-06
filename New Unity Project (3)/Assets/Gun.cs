f6ae915096fa3d835a9ac96dc6b6a188a8f00f1c
MohamedIsseAhmed <77129028+MohamedIsseAhmed@users.noreply.github.com>
2021-05-06 18:17:18 +0300

    Add Unity Project
    Add Unity Project

---
+++ New Unity Project (3)/Assets/Gun.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Transform projectilePosition;
+    public Projectile projectile;
+    public float projectileVelocity=35;
+    public float msecondsBetweenFires=100;
+    float nextFireTime;
+    
+    public void Shoot()
+    {
+        if (Time.time > nextFireTime)
+        {
+            nextFireTime = Time.time + msecondsBetweenFires / 1000;
+            Projectile projectileToFire = Instantiate(projectile, projectilePosition.position,projectilePosition.rotation) as Projectile;
+            projectileToFire.SetSpeed(projectileVelocity);
+        }
+       
+    }
+    
+}
---
+++ New Unity Project (3)/Assets/GunController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    Gun equipedGun;
+    public Transform weaponHolder;
+
+    public Gun startingGun;
+    void Start()
+    {
+        if (startingGun!=null)
+        {
+            EquipGun(startingGun);
+        }
+       
+    }
+
+   
+    void Update()
+    {
+        
+    }
+    public void EquipGun(Gun guntoEquip)
+    {
+        if (equipedGun!=null)
+        {
+            Destroy(equipedGun.gameObject);
+        }
+        equipedGun = Instantiate(guntoEquip, weaponHolder.position, Quaternion.identity) as Gun;
+        equipedGun.transform.parent = weaponHolder;
+    }
+    public void Shoot()
+    {
+        if (equipedGun!=null)
+        {
+            equipedGun.Shoot();
+        }
+    }
+}
