ca5495d1352d21dc5a7eefbe6063d9e361830ac5
MohamedIsseAhmed <77129028+MohamedIsseAhmed@users.noreply.github.com>
2021-05-06 18:25:04 +0300

    Edit Scripts

---
+++ New Unity Project (3)/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    Vector3 velocity;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+         velocity= _velocity ;
+    }
+    public void LookAt(Vector3 point)
+    {
+        Vector3 correctedLookPoint = new Vector3(point.x, transform.position.y, point.z);
+        transform.LookAt(correctedLookPoint);
+
+
+    }
+    public void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
